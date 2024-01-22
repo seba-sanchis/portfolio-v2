79e938bc4054c53f1f37ba21682e1f73ca229fb5
seba-sanchis <seba.sanchis@gmail.com>
2024-01-22 19:22:10 -0300

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 
 import { Navbar } from "@/components";
 import { getSocial } from "@/lib/actions";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Portfolio: Sebastian Sanchis",
@@ -22,6 +23,8 @@ export default async function RootLayout({
         <Navbar social={social} />
 
         <main className="flex-1 w-full">{children}</main>
+
+        <Analytics />
       </body>
     </html>
   );
